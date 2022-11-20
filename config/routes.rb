bcb61d7f710b8f57fd9a38bf98d657cc089ae178
playerzero <abingigo@gmail.com>
2022-11-20 10:33:22 +0530

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'login/index'
   resources :users
   resources :tags
   resources :expenses
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root 'login#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
 end
